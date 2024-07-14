f5930d310bd9b0fd7c946752bb085801470d8a4b
Nischal Subedi <nischal1337@gmail.com>
2024-07-14 12:07:44 +0545

    virtual function introduction

---
+++ learn/oops/polymerphism/intro.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Shape {
+protected:
+  int width, breadth;
+
+public:
+  Shape(int a = 0, int b = 0) {
+    width = a;
+    breadth = b;
+  }
+  virtual int area() {
+    cout << "the area is: " << endl;
+    return 0;
+  }
+};
+
+class Rectangle : public Shape {
+public:
+  Rectangle(int a = 0, int b = 0) : Shape(a, b) { Shape(a, b); }
+  int area() {
+    cout << "The area is: " << width * breadth << endl;
+    return 0;
+  }
+};
+
+class Triangle : public Shape {
+public:
+  Triangle(int a = 0, int b = 0) : Shape(a, b) { Shape(a, b); }
+  int area() {
+    cout << " The triangle area  is" << breadth * width / 2 << endl;
+    return 0;
+  }
+};
+
+int main() {
+  Shape *shape;
+  Rectangle rec(7, 10);
+  Triangle tri(7, 10);
+  shape = &rec;
+  shape->area();
+  shape = &tri;
+  shape->area();
+
+  return 0;
+}
