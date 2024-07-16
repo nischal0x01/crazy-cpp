3afb2dc05c5b05854058374343bfebd810f36ae3
Nischal Subedi <nischal1337@gmail.com>
2024-07-16 08:23:56 +0545

    templates in cpp using array

---
+++ learn/oops/templates/array.cpp
@@ -0,0 +1,29 @@
+// WAP to find the greatest value stored in array using function template
+
+#include <iostream>
+using namespace std;
+template <class T> T GetMax(T arr[], int size) {
+  int i;
+  T result = 0;
+  for (i = 0; i < size; i++) {
+    if (arr[i] > result) {
+      result = arr[i];
+    }
+  }
+  return result;
+};
+
+int main() {
+  int size;
+  cout << "Enter the size of array" << endl;
+  cin >> size;
+
+  cout << "Enter the numbers" << endl;
+  float arr[size];
+  for (int i = 0; i < size; i++) {
+    cin >> arr[i];
+  }
+  float result = GetMax<float>(arr, size);
+  cout << "The maximum number from the array is  " << result << endl;
+  return 0;
+}
